179f4b12ce1a86b581a67fd20469f2583d899e01
Dawid Świtalski <dawid.switalski100@wp.pl>
2018-02-07 19:18:30 +0100

    Buttons, Table

--- src/View/Invoices/Revenues/Revenues.jsx
+++ src/View/Invoices/Revenues/Revenues.jsx
@@ -1,13 +1,65 @@
 import React, { Component } from 'react';
 
-import { ContentContainer } from './Revenues_style'
+import { ContentContainer, Button } from './Revenues_style';
+
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
+
 
 export default class Revenues extends Component {
   render() {
       return(
         <ContentContainer>
-          <h1>Przychody</h1>
+          <Button label="WYSTAW NOWĄ FAKTURĘ" primary={true}></Button>
+          <Button label="EDYTUJ" primary={true}></Button>
+          <Button label="GENERUJ PDF" secondary={true}></Button>
+            <TableExampleSimple/>
         </ContentContainer>
       );
  }
 } 
+
+const TableExampleSimple = () => (
+  <Table>
+    <TableHeader displayHeaderCheckbox={false}>
+      <TableRow>
+        <TableHeaderColumn>ID</TableHeaderColumn>
+        <TableHeaderColumn>Name</TableHeaderColumn>
+        <TableHeaderColumn>Status</TableHeaderColumn>
+      </TableRow>
+    </TableHeader>
+    <TableBody displayRowCheckbox={false}>
+      <TableRow>
+        <TableRowColumn>1</TableRowColumn>
+        <TableRowColumn>John Smith</TableRowColumn>
+        <TableRowColumn>Employed</TableRowColumn>
+      </TableRow>
+      <TableRow>
+        <TableRowColumn>2</TableRowColumn>
+        <TableRowColumn>Randal White</TableRowColumn>
+        <TableRowColumn>Unemployed</TableRowColumn>
+      </TableRow>
+      <TableRow>
+        <TableRowColumn>3</TableRowColumn>
+        <TableRowColumn>Stephanie Sanders</TableRowColumn>
+        <TableRowColumn>Employed</TableRowColumn>
+      </TableRow>
+      <TableRow>
+        <TableRowColumn>4</TableRowColumn>
+        <TableRowColumn>Steve Brown</TableRowColumn>
+        <TableRowColumn>Employed</TableRowColumn>
+      </TableRow>
+      <TableRow>
+        <TableRowColumn>5</TableRowColumn>
+        <TableRowColumn>Christopher Nolan</TableRowColumn>
+        <TableRowColumn>Unemployed</TableRowColumn>
+      </TableRow>
+    </TableBody>
+  </Table>
+);
--- src/View/Invoices/Revenues/Revenues_style.jsx
+++ src/View/Invoices/Revenues/Revenues_style.jsx
@@ -1,5 +1,16 @@
 import styled from 'styled-components';
 
+import RaisedButton from 'material-ui/RaisedButton';
+
 export const ContentContainer = styled.div`
     margin: 10px;
 `;
+
+export const Button = styled(RaisedButton)`
+    margin: 5px;
+    display: inline;
+`
+
+
+
+
