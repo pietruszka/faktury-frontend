faa1251a4e4821204957bac255da124141441022
Przemek <przemek.rakowski@gmail.com>
2018-02-20 01:58:12 +0100

    .

--- src/Actions/Index.js
+++ src/Actions/Index.js
@@ -13,7 +13,7 @@ export const LOGIN_USER = 'LOGIN_USER';
 export const UPLOAD_FILE = 'UPLOAD_FILE';
 export const GENERATE_PDF = 'GENERATE_PDF';
 
-export const URL = 'http://localhost:3005/api';
+export const URL = '/api';
 
 
 export function fetchInvoices() {
